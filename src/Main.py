25a18f2e1054db80afaa867380b733b7a6a2b742
Sam Crawford <sam.crawford82@gmail.com>
2019-03-25 19:27:18 -0400

    Added masterlist and input mechanism

--- src/Main.py
+++ src/Main.py
@@ -1,7 +1,20 @@
 # Main.py
-# 3/24/2019
+# 3/25/2019
 
 def main():
-    print("Main")
+
+    masterList = [[chr(i) for i in range(ord("A"), ord("Z") + 1)] for j in range(27)]
+    masterList[0] = []
+
+    while True:
+        word = input("Type 'exit' to exit or a word: ")
+        if word == "exit":
+            break
+        else:
+            letters = word.split(" ")
+            print(letters)
+
+
+    print("\n Quit")
 
 main()

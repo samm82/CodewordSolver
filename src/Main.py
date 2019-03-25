00b1f358b0dba340f459294908b340c28f276a70
Sam Crawford <sam.crawford82@gmail.com>
2019-03-25 19:33:53 -0400

    Added some input handling and file reading

--- src/Main.py
+++ src/Main.py
@@ -7,12 +7,20 @@ def main():
     masterList[0] = []
 
     while True:
-        word = input("Type 'exit' to exit or a word: ")
+        word = input("Type 'exit' to exit or a codeword (separated by spaces): ")
         if word == "exit":
             break
+        elif word == "":
+            print("")
         else:
             letters = word.split(" ")
             print(letters)
+
+            file = open("words/words" + str(len(letters)).zfill(2) + ".txt", "r")
+            lines = file.readlines()
+            file.close()
+
+            print(lines[0])
 
 
     print("\n Quit")
